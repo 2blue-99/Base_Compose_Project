b994d7ba631df1d51bceefeaeaff8f2205f994eb
LeePureum <lpm083100@gmail.com>
2025-08-13 15:38:52 +0900

    gradle - Target, Compile SDK 버전 35 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.base_compose_project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.base_compose_project"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
